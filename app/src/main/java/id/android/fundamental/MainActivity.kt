d1e8544530d1ad872a479cb86464c69dcca9d357
ahmrh <ahmadrolandi01@gmail.com>
2023-10-28 10:17:18 +0700

    first commit

---
+++ app/src/main/java/id/android/fundamental/MainActivity.kt
@@ -0,0 +1,13 @@
+package id.android.fundamental
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(
+        savedInstanceState: Bundle?
+    ) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
